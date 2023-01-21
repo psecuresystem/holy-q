f426fa1a0115173974aff4966c4618fc4a2d3432
Vision <visiondaniels32@gmail.com>
2023-01-21 13:54:20 -0700

    Initialied Quantum Mech

--- src/CodeAnalysis/Binding/BoundLiteralExpression.ts
+++ src/CodeAnalysis/Binding/BoundLiteralExpression.ts
@@ -1,8 +1,9 @@
 import BoundExpression from './BoundExpression';
 import BoundNodeKind from './BoundNodeKind';
+import Types from './Types';
 
 export default class BoundLiteralExpression extends BoundExpression {
-  public type = 'number';
+  public type: Types = 'number';
   kind: BoundNodeKind = BoundNodeKind.LiteralExpression;
   constructor(readonly value: any) {
     super();
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import * as fs from 'fs';
-import Binder from './CodeAnalysis/Binding/Binder';
-import Evaluator from './CodeAnalysis/Evaluator';
-import Reporter from './CodeAnalysis/Report';
-import BinaryExpressionSyntax from './CodeAnalysis/Syntax/BinaryExpressionSyntax';
-import GlobalScopeSyntax from './CodeAnalysis/Syntax/GlobalScopeSyntax';
-import SyntaxTree from './CodeAnalysis/Syntax/SyntaxTree';
+// import * as fs from 'fs';
+// import Binder from './CodeAnalysis/Binding/Binder';
+// import Evaluator from './CodeAnalysis/Evaluator';
+// import Reporter from './CodeAnalysis/Report';
+// import BinaryExpressionSyntax from './CodeAnalysis/Syntax/BinaryExpressionSyntax';
+// import GlobalScopeSyntax from './CodeAnalysis/Syntax/GlobalScopeSyntax';
+// import SyntaxTree from './CodeAnalysis/Syntax/SyntaxTree';
 
-export const reporter = new Reporter();
-function main(entryPoint: string, showTree = false) {
-  fs.readFile(entryPoint, 'utf8', (err, data) => {
-    if (err) throw new Error(err.message);
-    const tree = SyntaxTree.parse(data);
-    const binder = new Binder();
-    const boundExp = binder.bindExpression(tree.root);
-    const evaluator = new Evaluator(boundExp);
-    showTree && console.log((tree.root as GlobalScopeSyntax).expressions);
-    evaluator.evaluate();
-    reporter.showErrors();
-  });
-}
+// export const reporter = new Reporter();
+// function main(entryPoint: string, showTree = false) {
+//   fs.readFile(entryPoint, 'utf8', (err, data) => {
+//     if (err) throw new Error(err.message);
+//     const tree = SyntaxTree.parse(data);
+//     const binder = new Binder();
+//     const boundExp = binder.bindExpression(tree.root);
+//     const evaluator = new Evaluator(boundExp);
+//     showTree && console.log((tree.root as GlobalScopeSyntax).expressions);
+//     evaluator.evaluate();
+//     reporter.showErrors();
+//   });
+// }
 
-main('./samples/expression.hq');
+// main('./samples/expression.hq');
+
+function main() {}
+
+main();
