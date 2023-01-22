e61eee8ba558566bc675deaf1c5fff63d8968a50
Vision <visiondaniels32@gmail.com>
2023-01-21 22:32:32 -0700

    Initialized Qubits

--- src/QuantumComputing/Qubit.ts
+++ src/QuantumComputing/Qubit.ts
@@ -1,3 +1,10 @@
 import Vector from './Math/Vector';
 
-export default class Qubit {}
+export default class Qubit {
+  constructor(public readonly state: Vector) {}
+
+  static fromBit(state: 0 | 1) {
+    if (state) return new Qubit(new Vector([0, 1]));
+    return new Qubit(new Vector([1, 0]));
+  }
+}
